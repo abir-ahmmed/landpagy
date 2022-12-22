04ed4b471dc34b73a542ac80d6ad3009d61f8624
abir-ahmmed <abirahmmedrana@outlook.com>
2022-12-22 11:14:29 +0600

    slider problem

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,29 @@
 const swiper = new Swiper('.swiper', {
-    speed: 400,
-    spaceBetween: 100,
-  });
+  // Default parameters
+  //  slidesPerView: 2,
+  loop:true,
+  spaceBetween: 10,
+  conteredSlides:true,
+  autoplay:{
+    delay:2500,
+    disableOnInteraction:false,
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 30
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1.5,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1.5,
+      spaceBetween: 40
+    }
+  }
+})
